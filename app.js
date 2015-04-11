35c5e644b2239737a834afc83e8ff8a219914cc1
fchau <de_generator@hotmail.com>
2015-04-11 00:18:33 -0700

    Add routing

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
+  , bitzplz = require('./routes/bitzplz')
   , http = require('http')
   , path = require('path');
 
@@ -16,7 +17,8 @@ var db = Mongoose.createConnection('localhost', 'mytestapp');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+//app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -32,7 +34,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-
+app.get('/bitzplz', bitzplz.bitzplz);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
