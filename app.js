128a1a9758a5f7e467104b6aadf3f608da6fbace
Jason Wu <me@jwu.io>
2015-04-11 02:16:16 -0700

    Added flickr route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
+  , flickr = require('./routes/flickr')
   , http = require('http')
   , path = require('path');
 
@@ -32,6 +33,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/flickr/mostRecent', flickr.mostRecent);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
