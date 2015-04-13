0cd3cbb1c63785edf61229dc611b02392b670e8b
fchau <de_generator@hotmail.com>
2015-04-12 21:13:02 -0700

    git wasn't working so here

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -2,46 +2,47 @@ var ctrl = angular.module('test', []);
 
 ctrl.controller('PostsCtrlAjax', function($scope, $http, $timeout){
   $scope.posts = [];
+  var rID = "";
   $scope.showloader = function() {
       $scope.state = "LOADING";
   }
-  $scope.getData = function(){
-    $http({method: 'GET', url: '../flickr/mostRecent'}).success(function(data)
-      {
-      $scope.posts = data.photo; // response data 
-      $scope.reportId = data._id; 
-      console.log($scope.posts);
-      $scope.state = "SUCCESS";
-      $scope.getColor();
-          $scope.colors = [];
 
-          angular.forEach(data.photo, function(value, dominantColor){
-            if($scope.colors.indexOf(value.dominantColor) == -1){
-              $scope.colors.push(value.dominantColor);
-            }
-            // console.log($scope.colors);
-          })
+  $scope.getData = function(){
+    $http({method: 'GET', url: '../flickr/mostRecent'}).success(function(data) {
+      $scope.posts = data.photo; // response data 
+      rID = data._id;
       });
    
   };
 $scope.getColor = function(){
-   $http({method: 'GET', url: '../flickr/' + $scope.reportId}).success(function(report){
+    console.log(rID);
+    $http({method: 'GET', url: '/flickr/' + rID}).success(function(report){
         $scope.posts = report.photo;
-         console.log($scope.posts);
-
+        $scope.colors = [];
+        $scope.state = "SUCCESS";
+        console.log($scope.colortest);
+        angular.forEach(report.photo, function(value, dominantColor){
+        if($scope.colors.indexOf(value.dominantColor) == -1 && value.dominantColor !== ''){
+           $scope.colors.push(value.dominantColor);
+        }
+        // console.log($scope.colors);
+        });
+        updateGraph(report);
     });
-}
+};
 $scope.intervalFunction = function(){
   setInterval(function() {
+    console.log("STARTING GETCOLOR");
     $scope.getColor();
   }, 10000);
 };
   //'javascripts/testapi.json'
   //'../flickr/mostRecent'
-  // Kick off the interval
+  // Kick off the interval 
 $scope.showloader();
 $scope.getData();
-//$scope.intervalFunction();
+// $scope.getColor();
+$scope.intervalFunction();
  // setInterval($scope.getData,5000);
  
  
--- public/javascripts/testdraw.js
+++ public/javascripts/testdraw.js
@@ -1,113 +1,106 @@
-var width = 500,
-    height = 500,
-    radius = Math.min(width, height) / 2,
-    innerRadius = 0.3 * radius;
+var updateGraph = function(report) {
+    var width = 500,
+        height = 500,
+        radius = Math.min(width, height) / 2,
+        innerRadius = 0.3 * radius,
+        modalElement = d3.select("#modaltest");
 
-var pie = d3.layout.pie()
-    .sort(null)
-    .value(function(d) { return d.width; });
+    modalElement.html('');
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function(d) { return d.width; });
 
-var tip = d3.tip()
-  .attr('class', 'd3-tip')
-  .offset([0, 0])
-  .html(function(d) {
-    return d.data.label + ": <span style='color:orangered'>" + d.data.score + "</span>";
-  });
-
-var arc = d3.svg.arc()
-  .innerRadius(innerRadius)
-  .outerRadius(function (d) { 
-    return (radius - innerRadius) * (d.data.score / 100.0) + innerRadius; 
-  });
-
-var outlineArc = d3.svg.arc()
-        .innerRadius(innerRadius)
-        .outerRadius(radius);
-
-var svg = d3.select("#modaltest").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-svg.call(tip);
-
-var data = {"foo":"bar"};
-
-d3.json('../flickr/mostRecent', function(data) {
-
-  var colorTotals = {};
-  var colorData = [];
-  console.log(data.photo);
-  data.photo.forEach(function(item, index) {
-    var color = item.dominantColor;
-    if (!colorTotals[color]) {
-      colorTotals[color] = 0;
-    }
-    colorTotals[color]++;
-  });
-
-  for (color in colorTotals) {
-    if (colorTotals.hasOwnProperty(color)) {
-      colorData.push({
-        id: color,
-        order: 1,
-        color: color,
-        weight: 100,
-        score: colorTotals[color],
-        width: 100,
-        label: color
+    var tip = d3.tip()
+      .attr('class', 'd3-tip')
+      .offset([0, 0])
+      .html(function(d) {
+        return d.data.label + ": <span style='color:orangered;'>" + d.data.score + "</span>";
       });
-    }
-  }
-  // data.forEach(function(d, index) {
-  //   d.id     =  d.name;
-  //   d.order  = +d.order;
-  //   d.color  =  d.dominant;
-  //   d.weight = +d.weight;
-  //   d.score  = +d.score;
-  //   d.width  = +d.weight;
-  //   d.label  =  d.name;
-  // });
-  // for (var i = 0; i < data.score; i++) { console.log(data[i].id) }
-  
-  var path = svg.selectAll(".solidArc")
-      // .data(pie(data))
-      .data(pie(colorData))
-    .enter().append("path")
-      .attr("fill", function(d) { return d.data.color; })
-      .attr("class", "solidArc")
-      .attr("stroke", "gray")
-      .attr("d", arc)
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide);
 
-  var outerPath = svg.selectAll(".outlineArc")
-      // .data(pie(data))
-      .data(pie(colorData))
-    .enter().append("path")
-      .attr("fill", "none")
-      .attr("stroke", "gray")
-      .attr("class", "outlineArc")
-      .attr("d", outlineArc);  
+    var arc = d3.svg.arc()
+      .innerRadius(innerRadius)
+      .outerRadius(function (d) {
+        return (radius - innerRadius) * (d.data.score / 100.0) + innerRadius;
+      });
+
+    var outlineArc = d3.svg.arc()
+            .innerRadius(innerRadius)
+            .outerRadius(radius);
+
+    var svg = d3.select("#modaltest").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    svg.call(tip);
 
 
-  // calculate the weighted mean score
-  var score = 
-    // data.reduce(function(a, b) {
-    colorData.reduce(function(a, b) {
-      //console.log('a:' + a + ', b.score: ' + b.score + ', b.weight: ' + b.weight);
-      return a + (b.score * b.weight); 
-    }, 0) / 
-    // data.reduce(function(a, b) { 
-    colorData.reduce(function(a, b) {
-      return a + b.weight; 
-    }, 0);
+    //d3.json(report, function(report) {
 
-  svg.append("svg:text")
-    .attr("class", "aster-score")
-    .attr("dy", ".35em")
-    .attr("text-anchor", "middle") // text-align: right
-    .text(Math.round(score));
+      var colorTotals = {};
+      var colorData = [];
+      console.log(report.photo);
+      report.photo.forEach(function(item) {
+        var color = item.dominantColor;
+        if (!colorTotals[color]) {
+          colorTotals[color] = 0;
+        }
+        colorTotals[color]++;
+      });
 
-});
+      for (color in colorTotals) {
+        if (colorTotals.hasOwnProperty(color)) {
+          colorData.push({
+            id: color,
+            order: 1,
+            color: color,
+            weight: 100,
+            score: colorTotals[color],
+            width: 100,
+            label: color
+          });
+        }
+      }
+
+      var path = svg.selectAll(".solidArc")
+          // .data(pie(data))
+          .data(pie(colorData))
+        .enter().append("path")
+          .attr("fill", function(d) { return d.data.color; })
+          .attr("class", "solidArc")
+          .attr("stroke", "gray")
+          .attr("d", arc)
+          .on('mouseover', tip.show)
+          .on('mouseout', tip.hide);
+
+      var outerPath = svg.selectAll(".outlineArc")
+          // .data(pie(data))
+          .data(pie(colorData))
+        .enter().append("path")
+          .attr("fill", "none")
+          .attr("stroke", "gray")
+          .attr("class", "outlineArc")
+          .attr("d", outlineArc);
+
+
+      // calculate the weighted mean score
+      var score =
+        // data.reduce(function(a, b) {
+        colorData.reduce(function(a, b) {
+          //console.log('a:' + a + ', b.score: ' + b.score + ', b.weight: ' + b.weight);
+          return a + (b.score * b.weight);
+        }, 0) /
+        // data.reduce(function(a, b) {
+        colorData.reduce(function(a, b) {
+          return a + b.weight;
+        }, 0);
+
+      svg.append("svg:text")
+        .attr("class", "aster-score")
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle") // text-align: right
+        .text(Math.round(score));
+
+    //});
+};
